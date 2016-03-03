79725c6819745898f88155fb7b5ac23598233891
Durian Monster <morita657@gmail.com>
2016-03-03 23:45:15 +0700

    Add humberger click function

--- js/google.js
+++ js/google.js
@@ -201,3 +201,17 @@ function deleteMarkers() {
   markers = [];
 }
 */
+      /*
+       * Open the drawer when the menu ison is clicked.
+       */
+      var menu = document.querySelector('#menu');
+      var main = document.querySelector('main');
+      var drawer = document.querySelector('#drawer');
+
+      menu.addEventListener('click', function(e) {
+        drawer.classList.toggle('open');
+        e.stopPropagation();
+      });
+      main.addEventListener('click', function() {
+        drawer.classList.remove('open');
+      });
