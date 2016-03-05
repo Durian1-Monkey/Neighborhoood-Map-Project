1b53ad6794d3e8c03a2c42316f7946a8995f631a
Durian Monster <morita657@gmail.com>
2016-03-05 17:51:15 +0700

    Update locations, lat and lng

--- js/google.js
+++ js/google.js
@@ -2,105 +2,107 @@ var map;
 var markers = [];
 var marker;
 var infoWindow;
-var bio = [
-{
-  'Name': 'BTS kokokok',
-        'lat': '37.769',
-        'lng': '-112.446',
-        'description':'description A'
-    },{
-        'Name': 'BTS',
-        'lat': '37.769',
-        'lng': '-102.446',
-        'description':'description B'
-    },{
-        'Name': 'MRT',
-        'lat': '37.769',
-        'lng': '-120.446',
-        'description':'description C'
-    },{
-        'Name': 'BTS',
-        'lat': '37.769',
-        'lng': '-122.446',
-        'description':'description D'
-    },{
-        'Name': 'JR',
-        'lat': '37.769',
-        'lng': '-120.446',
-        'description':'description D'
-    }];
-    /*
-var locations = [];
-var bioLength = bio.length;
-for(var i=0; i < bioLength; i++) {
-  locations.push(bio[i]);
+var bio = [{
+    'Name': 'Terminal 21',
+    'lat': '13.737920',
+    'lng': '100.560416',
+    'description': 'You can buy whatever you want here. </br>I often come here to buy books and small presents for my pals and family members. </br>This shopping mall is easy to access from BTS Asok.'
+}, {
+    'Name': 'SPICY BY NATURE',
+    'lat': '13.710273',
+    'lng': '100.510994',
+    'description': 'My favorite Indian restaurant. </br>The chef cooked amazing tasty Indian cuisines. </br>You can use a knife and fork here.'
+}, {
+    'Name': 'Lumphini Park',
+    'lat': '13.730026',
+    'lng': '100.541199',
+    'description': 'One of the biggest park in Bangkok. </br>People enjoy playing out here. </br>I love to sing and dance with my favorite musics.'
+}, {
+    'Name': 'Chatuchak Weekend Market',
+    'lat': '13.799994',
+    'lng': '100.550568',
+    'description': 'This is the big market in Bangkok. </br>You can find many interesting things which might be expensive if you bought at shopping malls'
+}, {
+    'Name': 'Wat Pho',
+    'lat': '13.746919',
+    'lng': '100.492738',
+    'description': 'My Thai friend invited me to go sightseeing here. </br>Inside the temple, the big guy is laying down to relax!! </br>And many people visit here to look at him every day.'
+}];
 
-//  Addmarker(bio[i]);
-}*/
 // In the following example, markers appear when the user clicks on the map.
 // The markers are stored in an array.
 // The user can then click an option to hide, show or delete the markers.
 
-
 function initMap() {
-  var haightAshbury = {lat: 37.769, lng: -122.446};
+    var haightAshbury = {
+        lat: 13.747174,
+        lng: 100.534907
+    };
 
-  map = new google.maps.Map(document.getElementById('map'), {
-    zoom: 12,
-    center: haightAshbury,
-    mapTypeId: google.maps.MapTypeId.TERRAIN
-  });
 
-  var bioLength = bio.length;
-  for(var i=0; i < bioLength; i++) {
-    var data = bio[i];
-  addMarker(data);
-}
-ko.applyBindings(new ViewModel());
+    map = new google.maps.Map(document.getElementById('map'), {
+        zoom: 12,
+        center: haightAshbury,
+        mapTypeId: google.maps.MapTypeId.TERRAIN,
+        mapTypeControl: true,
+    mapTypeControlOptions: {
+        style: google.maps.MapTypeControlStyle.HORIZONTAL_BAR,
+        position: google.maps.ControlPosition.RIGHT_TOP
+    }
+    });
+
+    var bioLength = bio.length;
+    for (var i = 0; i < bioLength; i++) {
+        var data = bio[i];
+        addMarker(data);
+    }
+    ko.applyBindings(new ViewModel());
 }
 
 // Adds a marker to the map and push to the array.
 function addMarker(loc) {
-  marker = new google.maps.Marker({
-    position: new google.maps.LatLng(loc.lat, loc.lng),
-    map: map,
-    title: loc.Name,
-    draggable: true,
-    animation: google.maps.Animation.DROP
-  });
+    marker = new google.maps.Marker({
+        position: new google.maps.LatLng(loc.lat, loc.lng),
+        map: map,
+        title: loc.Name,
+        draggable: true,
+        animation: google.maps.Animation.DROP
+    });
 
-  loc.marker = marker;
+    loc.marker = marker;
 
-  markers.push(marker);
+    markers.push(marker);
 
 
-        // infoWindows are the little helper windows that open when you click
-        // or hover over a pin on a map. They usually contain more information
-        // about a location.
-        var infoWindow = new google.maps.InfoWindow({
-            content: loc.Name + '<br>'+ '<br>' + loc.description
-        });
+    // infoWindows are the little helper windows that open when you click
+    // or hover over a pin on a map. They usually contain more information
+    // about a location.
+    var infoWindow = new google.maps.InfoWindow({
+        content: loc.Name + '<br>' + '<br>' + loc.description
+    });
 
-        // On click open the infoWindow
-        google.maps.event.addListener(marker, 'click', function() {
-            infoWindow.open(map, loc.marker);
-            toggleBounce();
-            test();
-        });
+    // On click open the infoWindow
+    google.maps.event.addListener(marker, 'click', function() {
+        infoWindow.open(map, loc.marker);
+        toggleBounce();
+        test();
+    });
 
-            function test(){
-              var cityStr = [];
-              cityStr.push(loc.Name);
-              console.log(cityStr);
-              loadData(cityStr);
+    function test() {
+        var cityStr = [];
+        cityStr.push(loc.Name);
+        console.log(cityStr);
+        loadData(cityStr);
 
-            }
-function toggleBounce() {
-  if (loc.marker.getAnimation() !== null) {
-    loc.marker.setAnimation(null);
-  } else {
-    loc.marker.setAnimation(google.maps.Animation.BOUNCE);
-  }}
+    }
+
+    function toggleBounce() {
+        if (loc.marker.getAnimation() !== null) {
+            loc.marker.setAnimation(null);
+        } else {
+            loc.marker.setAnimation(google.maps.Animation.BOUNCE);
+        }
+    }
 
 }
 
@@ -109,27 +111,27 @@ var ViewModel = function() {
     var self = this;
 
     //popped up massages after clicking
-    self.Locates = function(box1){
-  var ref;
-  for (var i = 0; i <bio.length ; i++) {
-    if(bio[i].Name == box1.Name) {
-      ref = markers[i];
-      var infoWindow = new google.maps.InfoWindow();
-      infoWindow.setContent(bio[i].Name + '<br>' + '<br>' + bio[i].description);
-      infoWindow.open(map, ref);
-      loadData(bio[i].Name);
-      box1.marker.setAnimation(google.maps.Animation.BOUNCE);
-    }
-  }
-};
+    self.Locates = function(box1) {
+        var ref;
+        for (var i = 0; i < bio.length; i++) {
+            if (bio[i].Name == box1.Name) {
+                ref = markers[i];
+                var infoWindow = new google.maps.InfoWindow();
+                infoWindow.setContent(bio[i].Name + '<br>' + '<br>' + bio[i].description);
+                infoWindow.open(map, ref);
+                loadData(bio[i].Name);
+                box1.marker.setAnimation(google.maps.Animation.BOUNCE);
+            }
+        }
+    };
 
     self.query = ko.observable('');
-    
-    self.search= ko.computed(function(){
-        return ko.utils.arrayFilter(bio, function(point){
-            if(point.Name.toLowerCase().indexOf(self.query().toLowerCase())>=0){
-              point.marker.setVisible(true);
-              console.log(point);
+
+    self.search = ko.computed(function() {
+        return ko.utils.arrayFilter(bio, function(point) {
+            if (point.Name.toLowerCase().indexOf(self.query().toLowerCase()) >= 0) {
+                point.marker.setVisible(true);
+                console.log(point);
 
                 return true;
             }
@@ -146,13 +148,13 @@ function loadData(str) {
     // clear out old data before new request
     $wikiElem.text("");
 
-var cityStr = str;
-console.log(cityStr);//Does this work on correctly?
+    var cityStr = str;
+    console.log(cityStr); //Does this work on correctly?
 
     // load wikipedia data
     var wikiUrl = 'http://en.wikipedia.org/w/api.php?action=opensearch&search=' + cityStr + '&format=json&callback=wikiCallback';
     console.log('loaded');
-    var wikiRequestTimeout = setTimeout(function(){
+    var wikiRequestTimeout = setTimeout(function() {
         $wikiElem.text("failed to get wikipedia resources");
     }, 8000);
 
@@ -160,7 +162,7 @@ console.log(cityStr);//Does this work on correctly?
         url: wikiUrl,
         dataType: "jsonp",
         jsonp: "callback",
-        success: function( response ) {
+        success: function(response) {
             var articleList = response[1];
             for (var i = 0; i < articleList.length; i++) {
                 articleStr = articleList[i];
@@ -175,43 +177,28 @@ console.log(cityStr);//Does this work on correctly?
 
 $('#form-container').submit(loadData);
 
+/* Open the drawer when the menu ison is clicked.*/
+var menu = document.querySelector('#menu');
+var main = document.querySelector('main');
+var drawer = document.querySelector('#drawer');
 
-/*
+menu.addEventListener('click', function(e) {
+    drawer.classList.toggle('open');
+    e.stopPropagation();
+});
+main.addEventListener('click', function() {
+    drawer.classList.remove('open');
+});
+//Resposive design. Sensor for iPhone and Android
+        function detectBrowser() {
+            var useragent = navigator.userAgent;
+            var mapdiv = document.getElementById("map");
 
-// Sets the map on all markers in the array.
-function setMapOnAll(map) {
-  for (var i = 0; i < markers.length; i++) {
-    markers[i].setMap(map);
-  }
-}
-
-// Removes the markers from the map, but keeps them in the array.
-function clearMarkers() {
-  setMapOnAll(null);
-}
-
-// Shows any markers currently in the array.
-function showMarkers() {
-  setMapOnAll(map);
-}
-
-// Deletes all markers in the array by removing references to them.
-function deleteMarkers() {
-  clearMarkers();
-  markers = [];
-}
-*/
-      /*
-       * Open the drawer when the menu ison is clicked.
-       */
-      var menu = document.querySelector('#menu');
-      var main = document.querySelector('main');
-      var drawer = document.querySelector('#drawer');
-
-      menu.addEventListener('click', function(e) {
-        drawer.classList.toggle('open');
-        e.stopPropagation();
-      });
-      main.addEventListener('click', function() {
-        drawer.classList.remove('open');
-      });
+            if (useragent.indexOf('iPhone') != -1 || useragent.indexOf('Android') != -1) {
+                mapdiv.style.width = '100%';
+                mapdiv.style.height = '100%';
+            } else {
+                mapdiv.style.width = '600px';
+                mapdiv.style.height = '800px';
+            }
+        }
