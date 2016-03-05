4ae538232a2d8f106d87f6afc584a752f672c6aa
Durian Monster <morita657@gmail.com>
2016-03-05 22:44:05 +0700

    Add new location points

--- js/google.js
+++ js/google.js
@@ -2,16 +2,16 @@ var map;
 var markers = [];
 var marker;
 var infoWindow;
-var bio = [{
+var locations = [{
     'Name': 'Terminal 21',
     'lat': '13.737920',
     'lng': '100.560416',
     'description': 'You can buy whatever you want here. </br>I often come here to buy books and small presents for my pals and family members. </br>This shopping mall is easy to access from BTS Asok.'
 }, {
-    'Name': 'SPICY BY NATURE',
-    'lat': '13.710273',
-    'lng': '100.510994',
-    'description': 'My favorite Indian restaurant. </br>The chef cooked amazing tasty Indian cuisines. </br>You can use a knife and fork here.'
+    'Name': 'Wat Arun',
+    'lat': '13.743719',
+    'lng': '100.489876',
+    'description': 'Entrance fee 50 BTH. </br>We should visit here in the early morning.'
 }, {
     'Name': 'Lumphini Park',
     'lat': '13.730026',
@@ -27,6 +27,16 @@ var bio = [{
     'lat': '13.746919',
     'lng': '100.492738',
     'description': 'My Thai friend invited me to go sightseeing here. </br>Inside the temple, the big guy is laying down to relax!! </br>And many people visit here to look at him every day.'
+}, {
+    'Name': 'Wat Suthat',
+    'lat': '13.752400',
+    'lng': '100.501120',
+    'description': 'This is a royal temple of the first grade. </br>I have never been to here yet.'
+}, {
+    'Name': 'Chao Phraya River',
+    'lat': '13.726736',
+    'lng': '100.512698',
+    'description': 'This is the first grade river in Thai. </br>You can enjoy a boat cruise tour.'
 }];
 
 // In the following example, markers appear when the user clicks on the map.
@@ -45,15 +55,15 @@ function initMap() {
         center: haightAshbury,
         mapTypeId: google.maps.MapTypeId.TERRAIN,
         mapTypeControl: true,
-    mapTypeControlOptions: {
-        style: google.maps.MapTypeControlStyle.HORIZONTAL_BAR,
-        position: google.maps.ControlPosition.RIGHT_TOP
-    }
+        mapTypeControlOptions: {
+            style: google.maps.MapTypeControlStyle.HORIZONTAL_BAR,
+            position: google.maps.ControlPosition.RIGHT_TOP
+        }
     });
 
-    var bioLength = bio.length;
-    for (var i = 0; i < bioLength; i++) {
-        var data = bio[i];
+    var locationsLength = locations.length;
+    for (var i = 0; i < locationsLength; i++) {
+        var data = locations[i];
         addMarker(data);
     }
     ko.applyBindings(new ViewModel());
@@ -113,13 +123,13 @@ var ViewModel = function() {
     //popped up massages after clicking
     self.Locates = function(box1) {
         var ref;
-        for (var i = 0; i < bio.length; i++) {
-            if (bio[i].Name == box1.Name) {
+        for (var i = 0; i < locations.length; i++) {
+            if (locations[i].Name == box1.Name) {
                 ref = markers[i];
                 var infoWindow = new google.maps.InfoWindow();
-                infoWindow.setContent(bio[i].Name + '<br>' + '<br>' + bio[i].description);
+                infoWindow.setContent(locations[i].Name + '<br>' + '<br>' + locations[i].description);
                 infoWindow.open(map, ref);
-                loadData(bio[i].Name);
+                loadData(locations[i].Name);
                 box1.marker.setAnimation(google.maps.Animation.BOUNCE);
             }
         }
@@ -128,7 +138,7 @@ var ViewModel = function() {
     self.query = ko.observable('');
 
     self.search = ko.computed(function() {
-        return ko.utils.arrayFilter(bio, function(point) {
+        return ko.utils.arrayFilter(locations, function(point) {
             if (point.Name.toLowerCase().indexOf(self.query().toLowerCase()) >= 0) {
                 point.marker.setVisible(true);
                 console.log(point);
@@ -190,15 +200,15 @@ main.addEventListener('click', function() {
     drawer.classList.remove('open');
 });
 //Resposive design. Sensor for iPhone and Android
-        function detectBrowser() {
-            var useragent = navigator.userAgent;
-            var mapdiv = document.getElementById("map");
+function detectBrowser() {
+    var useragent = navigator.userAgent;
+    var mapdiv = document.getElementById("map");
 
-            if (useragent.indexOf('iPhone') != -1 || useragent.indexOf('Android') != -1) {
-                mapdiv.style.width = '100%';
-                mapdiv.style.height = '100%';
-            } else {
-                mapdiv.style.width = '600px';
-                mapdiv.style.height = '800px';
-            }
-        }
+    if (useragent.indexOf('iPhone') != -1 || useragent.indexOf('Android') != -1) {
+        mapdiv.style.width = '100%';
+        mapdiv.style.height = '100%';
+    } else {
+        mapdiv.style.width = '600px';
+        mapdiv.style.height = '800px';
+    }
+}
