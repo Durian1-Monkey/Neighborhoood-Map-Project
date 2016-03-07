1459d9e57fabbcc9da78cede911a62b88a471034
Durian Monster <morita657@gmail.com>
2016-03-07 23:24:56 +0700

    Transfer wikipedia API inside the add marker function

--- src/js/google.js
+++ src/js/google.js
@@ -1,43 +1,43 @@
 var map,
-markers = [],
-marker,
-infoWindow,
-locations = [{
-    'Name': 'Terminal 21',
-    'lat': '13.737920',
-    'lng': '100.560416',
-    'description': 'You can buy whatever you want here. </br>I often come here to buy books and small presents for my pals and family members. </br>This shopping mall is easy to access from BTS Asok.'
-}, {
-    'Name': 'Wat Arun',
-    'lat': '13.743719',
-    'lng': '100.489876',
-    'description': 'Entrance fee 50 BTH. </br>We should visit here in the early morning.'
-}, {
-    'Name': 'Lumphini Park',
-    'lat': '13.730026',
-    'lng': '100.541199',
-    'description': 'One of the biggest park in Bangkok. </br>People enjoy playing out here. </br>I love to sing and dance with my favorite musics.'
-}, {
-    'Name': 'Chatuchak Weekend Market',
-    'lat': '13.799994',
-    'lng': '100.550568',
-    'description': 'This is the big market in Bangkok. </br>You can find many interesting things which might be expensive if you bought at shopping malls'
-}, {
-    'Name': 'Wat Pho',
-    'lat': '13.746919',
-    'lng': '100.492738',
-    'description': 'My Thai friend invited me to go sightseeing here. </br>Inside the temple, the big guy is laying down to relax!! </br>And many people visit here to look at him every day.'
-}, {
-    'Name': 'Wat Suthat',
-    'lat': '13.752400',
-    'lng': '100.501120',
-    'description': 'This is a royal temple of the first grade. </br>I have never been to here yet.'
-}, {
-    'Name': 'Chao Phraya River',
-    'lat': '13.726736',
-    'lng': '100.512698',
-    'description': 'This is the first grade river in Thai. </br>You can enjoy a boat cruise tour.'
-}];
+    markers = [],
+    marker,
+    infoWindow,
+    locations = [{
+        'Name': 'Terminal 21',
+        'lat': '13.737920',
+        'lng': '100.560416',
+        'description': 'You can buy whatever you want here. </br>I often come here to buy books and small presents for my pals and family members. </br>This shopping mall is easy to access from BTS Asok.'
+    }, {
+        'Name': 'Wat Arun',
+        'lat': '13.743719',
+        'lng': '100.489876',
+        'description': 'Entrance fee 50 BTH. </br>We should visit here in the early morning.'
+    }, {
+        'Name': 'Lumphini Park',
+        'lat': '13.730026',
+        'lng': '100.541199',
+        'description': 'One of the biggest park in Bangkok. </br>People enjoy playing out here. </br>I love to sing and dance with my favorite musics.'
+    }, {
+        'Name': 'Chatuchak Weekend Market',
+        'lat': '13.799994',
+        'lng': '100.550568',
+        'description': 'This is the big market in Bangkok. </br>You can find many interesting things which might be expensive if you bought at shopping malls'
+    }, {
+        'Name': 'Wat Pho',
+        'lat': '13.746919',
+        'lng': '100.492738',
+        'description': 'My Thai friend invited me to go sightseeing here. </br>Inside the temple, the big guy is laying down to relax!! </br>And many people visit here to look at him every day.'
+    }, {
+        'Name': 'Wat Suthat',
+        'lat': '13.752400',
+        'lng': '100.501120',
+        'description': 'This is a royal temple of the first grade. </br>I have never been to here yet.'
+    }, {
+        'Name': 'Chao Phraya River',
+        'lat': '13.726736',
+        'lng': '100.512698',
+        'description': 'This is the first grade river in Thai. </br>You can enjoy a boat cruise tour.'
+    }];
 
 // In the following example, markers appear when the user clicks on the map.
 // The markers are stored in an array.
@@ -65,6 +65,7 @@ function initMap() {
     for (var i = 0; i < locationsLength; i++) {
         var data = locations[i];
         addMarker(data);
+        //     loadData(data);
     }
     ko.applyBindings(new ViewModel());
 }
@@ -87,19 +88,22 @@ function addMarker(loc) {
     // infoWindows are the little helper windows that open when you click
     // or hover over a pin on a map. They usually contain more information
     // about a location.
-    
+    /*
     infoWindow = new google.maps.InfoWindow({
         content: loc.Name + '<br>' + '<br>' + loc.description
     });
-
+    */
+    infoWindow = new google.maps.InfoWindow();
 
     // On click open the infoWindow
     google.maps.event.addListener(marker, 'click', function() {
-        
-//        infoWindow.close();
-        infoWindow.open(map, loc.marker);
-        console.log("open");
+
+        //      infoWindow.close();
         toggleBounce();
+        infoWindow.setContent(loc.Name + '<br>' + '<br>' + loc.description + '<br>' + '<div id ="content">' + '</div>');
+        console.log("open");
+        loadData(loc.Name);
+        infoWindow.open(map, loc.marker);
         map.panTo(marker.getPosition());
     });
 
@@ -112,9 +116,43 @@ function addMarker(loc) {
             setTimeout(loc.marker.setAnimation(null), 700);
         }
     }
+    ///////////////////////////////////////////////////////////
 
+    //Wikipedia API
+    function loadData(str) {
+        var $wikiElem = $('#content');
+        // clear out old data before new request
+        // $wikiElem.text("");
+
+        //    var cityStr = str;
+
+        //    console.log(cityStr); //Does this work on correctly?
+
+        // load wikipedia data
+        //    var wikiUrl = 'http://en.wikipedia.org/w/api.php?action=opensearch&search=' + cityStr + '&format=json&callback=wikiCallback';
+        var wikiUrl = 'http://en.wikipedia.org/w/api.php?action=opensearch&search=' + str + '&format=json&callback=wikiCallback';
+        console.log(wikiUrl);
+        var wikiRequestTimeout = setTimeout(function() {
+            $wikiElem.text("failed to get wikipedia resources");
+        }, 8000);
+
+        $.ajax({
+            url: wikiUrl,
+            dataType: "jsonp",
+            jsonp: "callback",
+            success: function(response) {
+                var articleList = response[1];
+                for (var i = 0; i < articleList.length; i++) {
+                    articleStr = articleList[i];
+                    var url = 'http://en.wikipedia.org/wiki/' + articleStr;
+                    $wikiElem.append('<li><a href="' + url + '">' + articleStr + '</a></li>');
+                }
+                clearTimeout(wikiRequestTimeout);
+            }
+        });
+        return false;
+    }
 }
-
 var ViewModel = function() {
 
     var self = this;
@@ -126,12 +164,11 @@ var ViewModel = function() {
             if (locations[i].Name == box1.Name) {
                 ref = markers[i];
                 //infoWindow = new google.maps.InfoWindow();
-                infoWindow.setContent(locations[i].Name + '<br>' + '<br>' + locations[i].description);
-                infoWindow.open(map, ref);
-                loadData(locations[i].Name);
                 box1.marker.setAnimation(google.maps.Animation.BOUNCE);
-            //setTimeout(box1.marker.setAnimation(null), 700);
-            google.maps.event.trigger(ref, 'click');
+                //setTimeout(box1.marker.setAnimation(null), 700);
+                infoWindow.setContent(locations[i].Name + '<br>' + '<br>' + locations[i].description + '<br>' + '<div id ="content">' + '</div>');
+                infoWindow.open(map, ref);
+                google.maps.event.trigger(ref, 'click');
             }
         }
     };
@@ -151,40 +188,6 @@ var ViewModel = function() {
         });
     });
 };
-//Wikipedia API
-function loadData(str) {
-    var $wikiElem = $('#wikipedia-links');
-    // clear out old data before new request
-    $wikiElem.text("");
-
-    var cityStr = str;
-    console.log(cityStr); //Does this work on correctly?
-
-    // load wikipedia data
-    var wikiUrl = 'http://en.wikipedia.org/w/api.php?action=opensearch&search=' + cityStr + '&format=json&callback=wikiCallback';
-    console.log('loaded');
-    var wikiRequestTimeout = setTimeout(function() {
-        $wikiElem.text("failed to get wikipedia resources");
-    }, 8000);
-
-    $.ajax({
-        url: wikiUrl,
-        dataType: "jsonp",
-        jsonp: "callback",
-        success: function(response) {
-            var articleList = response[1];
-            for (var i = 0; i < articleList.length; i++) {
-                articleStr = articleList[i];
-                var url = 'http://en.wikipedia.org/wiki/' + articleStr;
-                $wikiElem.append('<li><a href="' + url + '">' + articleStr + '</a></li>');
-            }
-            clearTimeout(wikiRequestTimeout);
-        }
-    });
-    return false;
-}
-
-$('#form-container').submit(loadData);
 
 /* Open the drawer when the menu ison is clicked.*/
 var menu = document.querySelector('#menu');
