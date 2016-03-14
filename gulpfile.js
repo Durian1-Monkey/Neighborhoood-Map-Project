4381378bdec2c8058fbc336deec5c9cbd539fe30
Durian Monster <morita657@gmail.com>
2016-03-14 23:50:55 +0700

    Add some packages

--- gulpfile.js
+++ gulpfile.js
@@ -1,51 +1,82 @@
     
 var gulp = require('gulp'),
-    uglify = require('gulp-uglify'),
-    rename = require('gulp-rename'),
-    cssmin = require('gulp-cssmin'),
+  //  uglify = require('gulp-uglify'),
     jsmin = require('gulp-jsmin'),
-    htmlmin = require('gulp-htmlmin'),
-    critical = require('critical').stream;
+    cssmin = require('gulp-cssmin'),
+    rename = require('gulp-rename'),
+    uglify = require('gulp-uglify'),
+    livereload = require('gulp-livereload'),
+    browserSync = require('browser-sync').create();
 
 
 gulp.task('default', () => {
     console.log("Helo");
 });
 
-// Compiles scss files and outputs them to build/css/*.css
-gulp.task('styles', function(){
-	return gulp.src('./build/*.css')
-		.pipe(cssmin())
-		.pipe(rename({suffix: '.min'}))
-		.pipe(gulp.dest('./build/'));
 
-});
 
 // Concats & minifies js files and outputs them to build/js/app.js 
 gulp.task('scripts', function () {
-	gulp.src('./build/*.js')
-		.pipe(jsmin())
-		.pipe(rename({suffix: '.min'}))
-		.pipe(gulp.dest('./build/'));
+    gulp.src('src/js/google.js')
+        .pipe(uglify())
+        .pipe(jsmin())
+//        .pipe(uglify())
+        .pipe(gulp.dest('dist/js/'))
+        .pipe(livereload());
 });
 
-// Minifies our HTML files and outputs them to build/*.html
-gulp.task('html', function() {
-  return gulp.src('./build/*.html')
-    .pipe(htmlmin({collapseWhitespace: true}))
-		.pipe(rename({suffix: '.min'}))
-    .pipe(gulp.dest('./build/'))
+//Compress css file 
+gulp.task('styles', function () {
+    return gulp.src('src/style.css')
+    .pipe(cssmin())
+  //  .pipe(uglify())
+        .pipe(rename({suffix: '.min'}))
+    .pipe(gulp.dest('dist/'))
+    .pipe(livereload());
 });
 
-// Generate & Inline Critical-path CSS
-gulp.task('critical', function () {
-    return gulp.src('./build/*.html')
-        .pipe(critical({base: './', inline: true, css: ['./build/style.min.css']}))
-        .pipe(rename({suffix: '.critical'}))
-        .pipe(gulp.dest('./build/critical/'));
+//Watches JS
+gulp.task('watch', function() {
+
+    var server = livereload();
+
+    gulp.watch('src/js/google.js', ['scripts']);
+//    gulp.watch('src/*.html', ['scripts']);
+    gulp.watch('src/style.css', ['styles']);
 });
 
-gulp.task('default', ['styles', 'scripts', 'html']);
+
+
+
+/*
+// process JS files and return the stream.
+gulp.task('js', function () {
+    return gulp.src('src/js/*.js')
+        .pipe(uglify())
+        .pipe(gulp.dest('dist/js/'));
+});
+
+// create a task that ensures the `js` task is complete before
+// reloading browsers
+gulp.task('js-watch', ['js'], browserSync.reload);
+
+// use default task to launch Browsersync and watch JS files
+gulp.task('serve', ['js'], function () {
+
+    // Serve files from the root of this project
+    browserSync.init({
+        server: {
+            baseDir: "src/js/*.js"
+        }
+    });
+
+    // add browserSync.reload to the tasks array to make
+    // all browsers reload after tasks are complete.
+    gulp.watch("src/js/*.js", ['js-watch']);
+});
+*/
+//gulp.task('default', ['watch', 'js', 'js-watch', 'serve']);
+gulp.task('default', ['watch', 'scripts', 'styles']);
 
 
 
